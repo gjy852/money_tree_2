80ce7f679f2d14a94c603ffa807dddef7f7cf55c
Raghu Betina <raghu@firstdraft.com>
2016-12-04 15:02:01 +0000

    Whitelist columns in LiabilityType for ActiveAdmin

--- app/admin/liability_type.rb
+++ app/admin/liability_type.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register LiabilityType do
 
+ permit_params :credit_cards, :other_current_liabilities, :mortgages, :student_loans, :automobile_loans, :other_long_term_liabilities
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
