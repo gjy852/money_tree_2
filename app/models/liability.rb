f883cd4db5dfaa6eca27551d6ec94b7f52dbf685
Raghu Betina <raghu@firstdraft.com>
2016-12-04 15:01:32 +0000

    Direct association between liabilities and net_worths

--- app/models/liability.rb
+++ app/models/liability.rb
@@ -1,6 +1,10 @@
 class Liability < ApplicationRecord
   # Direct associations
 
+  has_one    :net_worth,
+             :foreign_key => "liabilities_total_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/net_worth.rb
+++ app/models/net_worth.rb
@@ -1,6 +1,9 @@
 class NetWorth < ApplicationRecord
   # Direct associations
 
+  belongs_to :liabilities_total,
+             :class_name => "Liability"
+
   has_many   :historical_net_worths,
              :foreign_key => "net_worth_total_id",
              :dependent => :destroy
