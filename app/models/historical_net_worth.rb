b7f9d9e98805ee22a6dca9a0962b860bbb2455a3
Raghu Betina <raghu@firstdraft.com>
2016-12-04 15:01:32 +0000

    Direct association between net_worths and historical_net_worths

--- app/models/historical_net_worth.rb
+++ app/models/historical_net_worth.rb
@@ -1,6 +1,9 @@
 class HistoricalNetWorth < ApplicationRecord
   # Direct associations
 
+  belongs_to :net_worth_total,
+             :class_name => "NetWorth"
+
   # Indirect associations
 
   # Validations
--- app/models/net_worth.rb
+++ app/models/net_worth.rb
@@ -1,6 +1,10 @@
 class NetWorth < ApplicationRecord
   # Direct associations
 
+  has_many   :historical_net_worths,
+             :foreign_key => "net_worth_total_id",
+             :dependent => :destroy
+
   belongs_to :asset_total,
              :class_name => "Asset"
 
