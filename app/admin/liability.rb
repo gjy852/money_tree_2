c8f96b3033b52fdb03d42002f65f0a60e5928984
Raghu Betina <raghu@firstdraft.com>
2016-12-04 15:01:58 +0000

    Whitelist columns in Liability for ActiveAdmin

--- app/admin/liability.rb
+++ app/admin/liability.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Liability do
 
+ permit_params :liabilities_type_id, :user_id, :liabilities_value
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
