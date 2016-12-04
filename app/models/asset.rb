416928f137c8a32c7a8daf0234fa4dbd8a208ccf
Raghu Betina <raghu@firstdraft.com>
2016-12-04 15:01:32 +0000

    Direct association between assets and net_worths

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,6 +1,10 @@
 class Asset < ApplicationRecord
   # Direct associations
 
+  has_one    :net_worth,
+             :foreign_key => "asset_total_id",
+             :dependent => :destroy
+
   belongs_to :asset_type
 
   # Indirect associations
--- app/models/net_worth.rb
+++ app/models/net_worth.rb
@@ -1,6 +1,9 @@
 class NetWorth < ApplicationRecord
   # Direct associations
 
+  belongs_to :asset_total,
+             :class_name => "Asset"
+
   # Indirect associations
 
   # Validations
